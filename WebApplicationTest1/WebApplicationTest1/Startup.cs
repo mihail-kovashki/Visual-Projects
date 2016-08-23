432f2dc8e695b6ac63772895550134009ce25577
mihail-kovashki <mihail.kovashki@yahoo.com>
2016-08-23 21:10:27 +0300

    Initial Upload

---
+++ WebApplicationTest1/WebApplicationTest1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationTest1.Startup))]
+namespace WebApplicationTest1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
